deb9f96856e05a1bdd08794aff3cb1dd94b2fdb5
matherheart <zjazsf@163.com>
2022-08-29 16:50:19 -0500

    Add files via upload
    new problems with hessian

--- Codes/problem/myfun3.m
+++ Codes/problem/myfun3.m
@@ -1,7 +1,9 @@
-function [z,gz] = myfun3(t)
+function [z,gz,H] = myfun3(t)
 x = t(1);
 y = t(2);
 z = (x^2+y^2-2)^2;
 if nargout > 1 % If a gradient is called for
-    gz = [4*x*(x^2+y^2-1) ; 4*y*(x^2+y^2-1) ];
+    gz = [4*x*(x^2+y^2-2) ; 4*y*(x^2+y^2-2) ];
+    H = [4*x*(x^2+y^2-2)+8*x^2, 8*x*y;
+        8*x*y, 4*x*(x^2+y^2-2)+8*y^2];
 end
--- Codes/problem/rosenbrockwithgrad.m
+++ Codes/problem/rosenbrockwithgrad.m
@@ -1,9 +1,12 @@
-function [f,g] = rosenbrockwithgrad(x)
+function [f,g,H] = rosenbrockwithgrad(x)
 % Calculate objective f
 f = 100*(x(2) - x(1)^2)^2 + (1-x(1))^2;
 
 if nargout > 1 % gradient required
     g = [-400*(x(2)-x(1)^2)*x(1) - 2*(1-x(1));
         200*(x(2)-x(1)^2)];
+    H = [-400*x(2) + 1200*x(1)^2 + 2, -400*x(1);
+        -400*x(1), 200];
 end
 end
+
